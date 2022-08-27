b3538781bfdc8ae8676980beb781a53a214e2f5a
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-27 17:38:54 +0530

    Update in code

--- describe_security_grp_rule.py
+++ describe_security_grp_rule.py
@@ -5,49 +5,48 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-AWS_REGION = input("Please enter the AWS_REGION")
+REGION = input("Please enter the REGION: ")
 
-# this is the configration for the logger
+# this is the configration for the logger_for
 
-logger = logging.getLogger()
+logger_for = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+client = boto3.client("ec2", region_name=REGION)
 
 
-def describe_rules(security_group_ids, max_items):
-    try:
-        
-        paginator = vpc_client.get_paginator('describe_security_group_rules')
+def describe_rules(grp_ids, maximum_items):
+    try: 
+        paginate = client.get_paginator('describe_security_group_rules')
 
-        response_iterator = paginator.paginate(
+        response_iterator = paginate.paginate(
             Filters=[{
                 'Name': 'group-id',
-                'Values': security_group_ids
+                'Values': grp_ids
             }],
-            PaginationConfig={'MaxItems': max_items})
+            PaginationConfig={'MaxItems': maximum_items})
 
-        full_result = response_iterator.build_full_result()
+        result = response_iterator.build_full_result()
 
-        security_groups_rules = []
+        grps_rules = []
 
-        for page in full_result['SecurityGroupRules']:
-            security_groups_rules.append(page)
+        for x in result['SecurityGroupRules']:
+            grps_rules.append(x)
 
     except ClientError:
-        logger.exception('This Security Groups Rules can not be described.')
+        logger_for.exception('This Security Groups Rules can not be described.')
         raise
     else:
-        return security_groups_rules
+        return grps_rules
 
 
 if __name__ == '__main__':
-    # SECURITY_GROUP_IDS = ['sg-022ed25b68ad24c18']
-    SECURITY_GROUP_IDS = input("Please enter the Security Groups ID")
-
-    MAX_ITEMS = 10
-    rules = describe_rules(SECURITY_GROUP_IDS, MAX_ITEMS)
-    logger.info('Security groups rules: ')
-    for rule in rules:
-        logger.info(json.dumps(rule, indent=4) + '\n')
+    # GRP_IDS = input("Please enter the Security Groups ID: ")
+    MAXIMUM_ITEMS = int(input("Enter the Value for MAX ITEMS: "))
+    GRP_IDS = ['<enter security grp id>']
+    grp_rules = describe_rules(GRP_IDS, MAXIMUM_ITEMS)
+    
+    logger_for.info('Your Security groups rules: ')
+    for x in grp_rules:
+        logger_for.info(json.dumps(x, indent=4) + '\n')
